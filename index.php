54ea605d4a06f9d07ab9cb1f1a619893ad144584
smelge <traildogfilms@gmail.com>
2015-08-30 19:24:12 +0100

    Fixing some fuckery

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 		<!-- Include all compiled plugins (below), or include individual files as needed -->
 		<script src="./js/bootstrap.min.js"></script>
-		<script src="jquery.bcSwipe.js"></script>
+		<script src="./js/jquery.bcSwipe.js"></script>
 	</head>
 	<body class="container-fluid laserbird">
 		<div id="main">
